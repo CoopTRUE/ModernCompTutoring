65b8950d25ebe1edbfb9a717545635baf7787db4
coopTRUE <coopdog4545@gmail.com>
2023-05-07 20:25:21 -0700

    initial files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Modern Comp Tutoring</title>
+  <meta
+    name="description"
+    content="Modern Comp Tutoring is a tutoring service for computer science students specializing in nearly all major programming languages."
+  />
+</svelte:head>
+
+<main>
+  <h1>hi</h1>
+</main>
+
+<style lang="scss">
+  h1 {
+    color: green;
+  }
+</style>
